74435485d084d215044f7b8d20932112a6ce31eb
moson-mo <mo-son@mailbox.org>
2019-08-31 12:44:20 +0200

    fix startscript

--- examples/start.sh
+++ examples/start.sh
@@ -3,5 +3,6 @@
 # run the install script and set this as startup/login script in your desktop environment (f.e. XFCE: "Session and Startup")
 # output will be logged to /tmp/smartlight.log, so you can check what is going on
 
+cd "$(dirname "${BASH_SOURCE[0]}")"
 ./service.sh &
 smtray &
